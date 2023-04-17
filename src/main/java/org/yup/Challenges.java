90acf38667d155917ea6da3b0f227687fa60ccb1
Eric Schwartz <erics273@gmail.com>
2023-04-17 15:59:49 -0400

    added several challenges

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -1,7 +1,61 @@
 package org.yup;
 
+import java.util.Scanner;
+
 public class Challenges {
 
-    
+    public String greeting(String name, int age){
+
+        //let folks into the party if they are 21 amd up
+        //politely tell them they cant enter under age
+        if(age >= 21){
+            return "Hello " + name + ", welcome to the the party!";
+        }else{
+            //this is what happens if the condition is false
+            return "Hello " + name + ", I am sorry but we cannot let you into the party";
+        }
+
+
+    }
+
+    public String greetingWithUserInput(){
+
+        Scanner theScanner = new Scanner(System.in);
+
+        //we need ask the user what their name is
+        System.out.print("Enter your name: ");
+        String name = theScanner.nextLine();
+
+        //we need to ask the user what their age
+        System.out.print("Enter your age: ");
+        int age = theScanner.nextInt();
+
+        //let folks into the party if they are 21 amd up
+        //politely tell them they cant enter under age
+        if(age >= 21){
+            return "Hello " + name + ", welcome to the the party!";
+        }else{
+            //this is what happens if the condition is false
+            return "Hello " + name + ", I am sorry but we cannot let you into the party";
+        }
+
+    }
+
+    public int addTwoNumbers(){
+
+        //ask the user for number1
+
+
+        //ask the user for number2
+
+
+        //return the sum of number1 and number 2 together
+
+        return 0;
+
+    }
+
+    //create a method like the above for subractTwoNumbers multiplyTwo numbers
+
 
 }
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -1,10 +1,33 @@
 package org.yup;
 
+//Doing to show you an import for now but this is not required since both are in the
+//package
+import org.yup.Challenges;
 public class JavaChallengesApp {
+
 
     public static void main (String[] args){
 
-        System.out.println("this is a test");
+        //this code is running greeting
+
+        //create a new instance of the challenges class so we can use the challenges in our application
+        Challenges challenges = new Challenges();
+
+        //this runs the greeting method from our challenges and returns a string greeting and
+        //stores it in the greeting variable we created
+        String greeting = challenges.greeting("eric",20);
+
+        //this prints the greeting to the console
+        System.out.println(greeting);
+
+        //code below is running greetingWithUserInput()
+        String greetingWithUserInput = challenges.greetingWithUserInput();
+
+        System.out.println(greetingWithUserInput);
+
+
+
+
 
     }
 
